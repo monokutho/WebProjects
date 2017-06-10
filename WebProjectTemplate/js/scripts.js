c8f3bae30b20f533e256068566a257f11c2a3311
monokutho <monokutho@gmail.com>
2017-06-10 17:26:34 +0200

    JScalculator creada

---
+++ WebProjectTemplate/js/scripts.js
@@ -0,0 +1,42 @@
+var result = 0;
+var acc = 0;
+var current = 0;
+
+function userInput() {
+    
+}
+
+function add() {
+    
+}
+
+function substract() {
+    
+}
+
+function multiply() {
+    
+}
+
+function divide() {
+    
+}
+
+function equal() {
+    
+}
+
+function point() {
+    
+}
+
+function allClear() {
+    
+}
+
+function clearEntry() {
+    
+}
+
+
+
